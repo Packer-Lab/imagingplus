8e3d61f16a634b313daca3868035eb353af30fb3
prajayshah <prajay.shah@mail.utoronto.ca>
2022-10-22 13:21:17 -0400

    updated html docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ exclude_patterns = ['build', '**.ipynb_checkpoints']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
 html_logo = 'files/imagingplus-logo.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
