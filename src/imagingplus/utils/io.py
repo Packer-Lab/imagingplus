68e987a866598edd22c07eb8010458da3a288490
prajayshah <prajay.shah@mail.utoronto.ca>
2022-12-10 23:05:21 -0500

    temp workarounds for importing errors after renaming of package

--- src/imagingplus/utils/io.py
+++ src/imagingplus/utils/io.py
@@ -6,6 +6,26 @@ import pandas as pd
 pd.options.display.max_rows = 100
 pd.options.display.max_columns = 10
 
+class CustomUnpicklerModuleNotFoundError(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'packerlabimaging.main.paq':
+            print(f'replacing {module}')
+            renamed_module = "imagingplus.processing.paq"
+        elif module == 'packerlabimaging':
+            renamed_module = "imagingplus"
+        elif module == 'packerlabimaging.processing.imagingMetadata':
+            renamed_module = "imagingplus.processing.imagingMetadata"
+        elif module == 'packerlabimaging.main.paq':
+            renamed_module = "imagingplus.main.paq"
+        elif 'packerlabimaging' in module:
+            print(f'replacing {module}')
+            renamed_module = module.replace('packerlabimaging', 'imagingplus')
+            # renamed_module = 'imagingplus'
+        else:
+            renamed_module = module
+
+        return super().find_class(renamed_module, name)
+
 
 # import .pkl'd objects
 def import_obj(pkl_path):
@@ -28,6 +48,10 @@ def import_obj(pkl_path):
             obj = pickle.load(f)
         except pickle.UnpicklingError:
             raise pickle.UnpicklingError(f"\n** FAILED IMPORT from {pkl_path}\n")
+        except ModuleNotFoundError:
+            print(f"WARNING: needing to try using CustomUnpickler!")
+            obj = CustomUnpicklerModuleNotFoundError(open(pkl_path, 'rb')).load()
+
         print(f'|- Loaded {obj.__repr__()}\n')
 
     return obj
